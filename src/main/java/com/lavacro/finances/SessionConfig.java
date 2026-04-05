d98d8b80f403f638f965490838727e03bc54c449
David Rosario <david.rosario69@gmail.com>
2026-04-05 18:23:41 -0400

    don't need indexed session config

--- src/main/java/com/lavacro/finances/SessionConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lavacro.finances;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
-
-@Configuration
-@EnableRedisIndexedHttpSession(maxInactiveIntervalInSeconds = 600, redisNamespace = "finances")
-public class SessionConfig {
-}
