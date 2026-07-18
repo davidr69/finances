f270c4dde5cfa29d711f67d19477e98d2ebdac97
David Rosario <david@lavacro.com>
2026-07-18 01:16:49 -0400

    cleanup

--- src/main/java/com/lavacro/finances/config/SecurityConfig.java
+++ src/main/java/com/lavacro/finances/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.config.http.SessionCreationPolicy;
